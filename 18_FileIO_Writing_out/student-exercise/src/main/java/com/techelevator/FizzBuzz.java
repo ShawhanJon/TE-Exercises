5c5c21a7f7c335760626249e4258c426869c2cb6
Jon Barnette <joncbarnette@gmail.com>
2021-02-03 15:48:23 -0500

    Exercise 18 Complete

--- 18_FileIO_Writing_out/student-exercise/src/main/java/com/techelevator/FizzBuzz.java
+++ 18_FileIO_Writing_out/student-exercise/src/main/java/com/techelevator/FizzBuzz.java
@@ -1,11 +1,10 @@
 package com.techelevator;
 
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 
-public class FizzWriter {
+public class FizzBuzz {
 
 	public static void main(String[] args) {
 
@@ -13,19 +12,13 @@ public class FizzWriter {
 		String output;
 
 		try (PrintWriter writer = new PrintWriter(fizzBuzz)) {
-			for (int i = 1; i < 301; i++) {
-				if (i % 15 == 0) {
+			for (int i = 1; i <= 300; i++) {
+				if (i % 15 == 0 || (Integer.toString(i).contains("3")) && (Integer.toString(i).contains("5"))) {
 					output = "FizzBuzz";
-				} else if (i % 5 == 0) {
+				} else if (i % 5 == 0 || (Integer.toString(i).contains("5"))) {
 					output = "Buzz";
-				} else if (i % 3 == 0) {
+				} else if (i % 3 == 0 || (Integer.toString(i).contains("3"))) {
 					output = "Fizz";
-				} else if ((Integer.toString(i).contains("3")) && (Integer.toString(i).contains("5"))) {
-					output = "FizzBuzz";
-				} else if (Integer.toString(i).contains("3")) {
-					output = "Fizz";
-				} else if (Integer.toString(i).contains("5")) {
-					output = "Buzz";
 				} else {
 					output = Integer.toString(i);
 				}
@@ -37,5 +30,7 @@ public class FizzWriter {
 			System.out.println("");
 			System.exit(1);
 		}
-}
+
+	}
+
 }
