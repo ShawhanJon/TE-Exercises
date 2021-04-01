a9ee8e250f634419234ed8a0a0930489a6642cb8
Jon Barnette <me@jonbarnette.com>
2021-04-01 08:39:13 -0400

    M3 Ex 13 Re-Done!

--- module-3/13_Vue_Component_Communication/student-exercise/src/components/BookCard.vue
+++ module-3/13_Vue_Component_Communication/student-exercise/src/components/BookCard.vue
@@ -1,21 +1,23 @@
 <template>
-  <div class="card" v-bind:class="{read:book.read}">
-    <h2 class="book-title">{{book.title}}</h2>
+  <div class="card" v-bind:class="{ read: book.read }">
+    <h2 class="book-title">{{ book.title }}</h2>
     <img v-if="book.isbn" v-bind:src="'http://covers.openlibrary.org/b/isbn/' + book.isbn + '-M.jpg'" />
-    <h3 class="book-author">{{book.author}}</h3>
-    <button class="mark-unread" v-if="book.read" v-on:click.prevent="markReadOrUnread(false)">Mark Unread</button>
-    <button class="mark-read" v-if="!book.read" v-on:click.prevent="markReadOrUnread(true)">Mark Read</button>
+    <h3 class="book-author">{{ book.author }}</h3>
+    <button class="mark-read" v-on:click.prevent="setRead(true)" v-if=" ! book.read">Mark Read</button>
+    <button class="mark-unread" v-on:click.prevent="setRead(false)" v-if="book.read">Mark Unread</button>
   </div>
 </template>
 
 <script>
 export default {
     name: 'book-card',
-    props: ['book'],
+    props: {
+        book: Object
+    },
     methods: {
-        markReadOrUnread(value) {
-            this.$store.commit('MARK_READ_OR_UNREAD', {book: this.book, value: value});
-        },
+        setRead(value) {
+            this.$store.commit('SET_READ_STATUS', {book: this.book, value: value});
+        }
     }
 }
 </script>
